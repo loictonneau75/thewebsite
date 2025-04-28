9c95707a35803cb60f58796a7c195102cc1a6224
Loic tonneau <loictonneau75@gmail.com>
2025-04-28 15:34:43 +0200

    ajout du container pour le form et le titre

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
 import { create_form, attachFormHandler } from "./form/index.js";
 import { displayTeas } from "./visualisation.js";
 
+function spacer(height = 2) {
+    height = `${height.toString()}rem`
+    const spacer = document.createElement('div');
+    spacer.style.height = height;
+    spacer.style.backgroundColor = 'transparent';
+    spacer.style.width = '100%';
+    return spacer;
+}
+
 function setup_doc(name) {
     document.title = name;
     const h1 = document.createElement('h1');
     h1.innerText = name;
-    document.body.appendChild(h1);
-    document.body.appendChild(create_form())
+    h1.classList.add('text-center', 'mb-4');
+
+    const mainWrapper = document.createElement('div');
+    mainWrapper.classList.add("bg-white", 'container-fluid', 'p-5');
+
+    mainWrapper.appendChild(h1);
+    mainWrapper.appendChild(create_form());
+    
+    document.body.appendChild(spacer())
+    document.body.appendChild(mainWrapper);
+
     attachFormHandler()
+
     document.body.appendChild(displayTeas())
 
 };
