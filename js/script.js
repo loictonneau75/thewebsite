0d1bc18bf4439965b7edb2baa6c626d6868199ac
loictonneau75 <loictonneau75@gmail.com>
2025-05-01 11:09:43 +0200

    changement de grid

--- js/script.js
+++ js/script.js
@@ -9,14 +9,14 @@ function setup_doc(name) {
     h1.classList.add('text-center');
 
     const formWrapper = document.createElement('div');
-    formWrapper.classList.add('container-fluid', 'p-5', "bg-secondary", "my-5");
+    formWrapper.classList.add('container', 'p-5', "bg-secondary", "my-5");
     formWrapper.append(h1, create_form());
 
     const h2 = document.createElement("h2");
     h2.innerText = "Liste de mes thés";
 
     const teaViewWrapper = document.createElement('div');
-    teaViewWrapper.classList.add("container-fluid", "p-5")
+    teaViewWrapper.classList.add("container", "p-5", "bg-secondary")
     teaViewWrapper.append(h2, displayTeas())
 
     document.body.append(formWrapper, teaViewWrapper);
